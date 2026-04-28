36d24784e742c45d6eaa1a42d10e3843466e57c1
Shahriar-Hossein <shahriar2507@gmail.com>
2026-04-28 22:52:43 +0600

    remove unnecessary things

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,20 +13,20 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
+      // {
+      //   url: '/icon-light-32x32.png',
+      //   media: '(prefers-color-scheme: light)',
+      // },
+      // {
+      //   url: '/icon-dark-32x32.png',
+      //   media: '(prefers-color-scheme: dark)',
+      // },
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/logo-theatre.jpg',
+        type: 'image/jpg',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo-theatre.jpg',
   },
 }
 
